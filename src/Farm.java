efe564c5c6674682fc9bf2f8ef380c31fe84bd65
kvasnicka <kvasnicka@spsejecna.cz>
2024-03-08 13:36:07 +0100

    pridany metody pro pridani a odebrani rostlin a zvirat

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,42 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Farm {
+
+    ArrayList<Animal> zviratka = new ArrayList<>();
+    ArrayList<Flower> rostlinky = new ArrayList<>();
+
+    public void addAnimal(Animal animal) throws Exception {
+        if (zviratka.size() <= 20) {
+            zviratka.add(animal);
+        } else {
+            throw new Exception("Maximalni pocet zvirat. Nemuzes pridavat dalsi");
+        }
+    }
+
+    public void removeAnimal(int index) throws Exception {
+        if (index > 0) {
+            throw new Exception();
+        } else {
+            zviratka.remove(index);
+        }
+    }
+
+    public void addFlower(Flower flower) throws Exception {
+            if (rostlinky.size() <= 5) {
+                rostlinky.add(flower);
+            } else {
+                throw new Exception("Maximalni pocet rosltin. Nemuzes pridavat dalsi");
+            }
+        }
+
+        public void removeFlower(int index) throws Exception {
+        if (index > 0) {
+            throw new Exception();
+        } else {
+            rostlinky.remove(index);
+        }
+    }
 
 
 
